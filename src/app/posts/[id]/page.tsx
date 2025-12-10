a2a527e89e98949021d6e525d2c16d2f798b916c
zunobk <junho4560@naver.com>
2025-12-10 16:34:26 +0900

    가드 클로즈(guard clause) 패턴을 적용하여 소스코드를 조금 더 깔끔하게

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
-export default function Page() {
-  const { id } = useParams<{ id: string }>();
+export default function Page({ params }: { params: Promise<{ id: number }> }) {
+  const { id } = use(params);
+
   const [post, setPost] = useState<{
     id: number;
     title: string;
@@ -17,19 +17,15 @@ export default function Page() {
       .then(setPost);
   }, []);
 
+  if (post == null) return <div>로딩중...</div>;
+
   return (
     <>
       <h1>글 상세페이지</h1>
 
-      {post == null && <div>로딩중...</div>}
-
-      {post != null && (
-        <>
-          <div>번호 : {post.id}</div>
-          <div>제목: {post.title}</div>
-          <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
-        </>
-      )}
+      <div>번호 : {post.id}</div>
+      <div>제목: {post.title}</div>
+      <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
     </>
   );
 }
