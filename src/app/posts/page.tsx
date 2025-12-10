9f566aaf20e3c1e076ec3ce8d908be841d080b89
zunobk <junho4560@naver.com>
2025-12-10 14:06:39 +0900

    하드코딩 방식으로 데이터 생성하여 반복문으로 렌더링

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,13 +1,27 @@
 "use client";
 
 export default function Page() {
+  const posts = [
+    { id: 1, title: "명언 1" },
+    { id: 2, title: "명언 2" },
+    { id: 3, title: "명언 3" },
+    { id: 4, title: "명언 4" },
+    { id: 5, title: "명언 5" },
+    { id: 6, title: "명언 6" },
+    { id: 7, title: "명언 7" },
+    { id: 8, title: "명언 8" },
+    { id: 9, title: "명언 9" },
+    { id: 10, title: "명언 10" },
+  ];
+
   return (
     <>
       <h1>글 목록</h1>
 
       <ul>
-        <li>명언 1</li>
-        <li>명언 2</li>
+        {posts.map(post => (
+          <li key={post.id}>{post.title}</li>
+        ))}
       </ul>
     </>
   );
