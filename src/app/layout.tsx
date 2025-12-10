6fa76149fe16ebb6ba8cddd28f7268ec0edc2e88
zunobk <junho4560@naver.com>
2025-12-10 11:39:56 +0900

    오직 body 엘리먼트만 배경색이 본인의 영역을 벗어나서 화면을 가득 채우게 됩니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased !bg-red-500 border-2 border-blue-500`}
       >
         <header>
           <nav className="flex">
@@ -38,7 +38,7 @@ export default function RootLayout({
             </Link>
           </nav>
         </header>
-        <main>{children}</main>
+        {children}
         <footer>푸터</footer>
       </body>
     </html>
