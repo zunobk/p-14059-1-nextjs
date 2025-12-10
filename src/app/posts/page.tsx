68f2d38c379cef3da7775961d9ebb86aa1b8d30b
zunobk <junho4560@naver.com>
2025-12-10 14:07:23 +0900

    fetch는 딱 1번 수행되어야 하기 때문에 useEffect 사용, posts의 상태는 빈 배열에서 변경되니 state 변수로 생성

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,25 +1,24 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
 export default function Page() {
-  const posts = [
-    { id: 1, title: "명언 1" },
-    { id: 2, title: "명언 2" },
-    { id: 3, title: "명언 3" },
-    { id: 4, title: "명언 4" },
-    { id: 5, title: "명언 5" },
-    { id: 6, title: "명언 6" },
-    { id: 7, title: "명언 7" },
-    { id: 8, title: "명언 8" },
-    { id: 9, title: "명언 9" },
-    { id: 10, title: "명언 10" },
-  ];
+  const [posts, setPosts] = useState<{ id: number; title: string }[]>([]);
+
+  useEffect(() => {
+    setPosts([
+      { id: 1, title: "명언 1" },
+      { id: 2, title: "명언 2" },
+      { id: 3, title: "명언 3" },
+    ]);
+  }, []);
 
   return (
     <>
       <h1>글 목록</h1>
 
       <ul>
-        {posts.map(post => (
+        {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
       </ul>
