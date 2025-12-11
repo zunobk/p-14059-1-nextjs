46266b5892970b0c01c54cd5bb8b6ab15e160d7f
zunobk <junho4560@naver.com>
2025-12-11 14:47:44 +0900

    글과 댓글의 다건조회에서 관련 상태의 초기값이 null 이어야 하는 이유

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -6,26 +6,30 @@ import Link from "next/link";
 import { useEffect, useState } from "react";
 
 export default function Page() {
-  const [posts, setPosts] = useState<PostDto[]>([]);
+  const [posts, setPosts] = useState<PostDto[] | null>(null);
 
   useEffect(() => {
     apiFetch(`/api/v1/posts`)
       .then(setPosts);
   }, []);
 
+  if (posts == null) return <div>로딩중...</div>;
+
   return (
     <>
       <h1>글 목록</h1>
 
-      {posts.length == 0 && <div>로딩중...</div>}
+      {posts.length == 0 && <div>글이 없습니다.</div>}
 
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Link href={`/posts/${post.id}`}>{post.title}</Link>
-          </li>
-        ))}
-      </ul>
+      {posts.length > 0 && (
+        <ul>
+          {posts.map((post) => (
+            <li key={post.id}>
+              <Link href={`/posts/${post.id}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      )}
 
       <div>
         <Link href="/posts/write">글쓰기</Link>
