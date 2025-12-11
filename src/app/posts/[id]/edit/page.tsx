73a6e61f6ebb914cfcf7d9ad49d11a997b49090b
zunobk <junho4560@naver.com>
2025-12-11 14:08:00 +0900

    글 수정 폼 구현, 수정 폼에 기본적으로 글 제목, 내용이 입력되도록

--- src/app/posts/[id]/edit/page.tsx
+++ src/app/posts/[id]/edit/page.tsx
@@ -1,9 +1,43 @@
 "use client";
 
-import { use } from "react";
+import { apiFetch } from "@/lib/backend/client";
+import { PostWithContentDto } from "@/type/post";
+import { use, useEffect, useState } from "react";
 
 export default function Page({ params }: { params: Promise<{ id: number }> }) {
   const { id } = use(params);
 
-  return <div>{id}번 글 수정페이지</div>;
+  const [post, setPost] = useState<PostWithContentDto | null>(null);
+
+  useEffect(() => {
+    apiFetch(`/api/v1/posts/${id}`).then(setPost);
+  }, []);
+
+  if (post == null) return <div>로딩중...</div>;
+
+  return (
+    <>
+      <h1>{id}번 글 수정</h1>
+
+      <form className="flex flex-col gap-2 p-2">
+        <input
+          className="border p-2 rounded"
+          type="text"
+          name="title"
+          placeholder="제목"
+          autoFocus
+          defaultValue={post.title}
+        />
+        <textarea
+          className="border p-2 rounded"
+          name="content"
+          placeholder="내용"
+          defaultValue={post.content}
+        />
+        <button className="border p-2 rounded" type="submit">
+          저장
+        </button>
+      </form>
+    </>
+  );
 }
