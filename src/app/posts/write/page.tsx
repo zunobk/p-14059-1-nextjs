eebe6b0fe8ba9656ca55ea74954922340fc8cbd3
zunobk <junho4560@naver.com>
2025-12-11 11:09:40 +0900

    글 쓰기 페이지 추가, 글 쓰기 폼 추가, 유효성 체크

--- src/app/posts/write/page.tsx
+++ src/app/posts/write/page.tsx
@@ -1,3 +1,53 @@
+"use client";
+
 export default function Page() {
-  return <div>글쓰기</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const form = e.target as HTMLFormElement;
+
+    const titleInput = form.elements.namedItem("title") as HTMLInputElement;
+    const contentTextarea = form.elements.namedItem(
+      "content"
+    ) as HTMLTextAreaElement;
+
+    titleInput.value = titleInput.value.trim();
+
+    if (titleInput.value.length === 0) {
+      alert("제목을 입력해주세요.");
+      titleInput.focus();
+      return;
+    }
+
+    contentTextarea.value = contentTextarea.value.trim();
+
+    if (contentTextarea.value.length === 0) {
+      alert("내용을 입력해주세요.");
+      contentTextarea.focus();
+      return;
+    }
+  };
+
+  return (
+    <>
+      <h1>글쓰기</h1>
+
+      <form className="flex flex-col gap-2 p-2" onSubmit={handleSubmit}>
+        <input
+          className="border p-2 rounded"
+          type="text"
+          name="title"
+          placeholder="제목"
+        />
+        <textarea
+          className="border p-2 rounded"
+          name="content"
+          placeholder="내용"
+        />
+        <button className="border p-2 rounded" type="submit">
+          저장
+        </button>
+      </form>
+    </>
+  );
 }
