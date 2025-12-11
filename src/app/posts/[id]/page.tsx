ce1c445fb1e91b0e780801e33f870cd2b93112a1
zunobk <junho4560@naver.com>
2025-12-11 14:37:08 +0900

    댓글 다건조회

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,17 +1,17 @@
 "use client";
 
 import { apiFetch } from "@/lib/backend/client";
-import type { PostWithContentDto } from "@/type/post";
-import { useEffect, useState } from "react";
-import { useParams, useRouter } from "next/navigation";
+import type { PostCommentDto, PostWithContentDto } from "@/type/post";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
-export default function Page() {
-  const { id: idStr } = useParams<{ id: string }>();
-  const id = Number(idStr);
+export default function Page({ params }: { params: Promise<{ id: number }> }) {
+  const { id } = use(params);
   const router = useRouter();
 
   const [post, setPost] = useState<PostWithContentDto | null>(null);
+  const [postComments, setPostComments] = useState<PostCommentDto[] | null>(null);
 
   const deletePost = (id: number) => {
     apiFetch(`/api/v1/posts/${id}`, {
@@ -23,8 +23,9 @@ export default function Page() {
   };
 
   useEffect(() => {
-    apiFetch(`/api/v1/posts/${id}`)
-      .then(setPost);
+    apiFetch(`/api/v1/posts/${id}`).then(setPost);
+
+    apiFetch(`/api/v1/posts/${id}/comments`).then(setPostComments);
   }, []);
 
   if (post == null) return <div>로딩중...</div>;
@@ -40,7 +41,10 @@ export default function Page() {
       <div className="flex gap-2">
         <button
           className="p-2 rounded border"
-          onClick={() => confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) && deletePost(post.id)}
+          onClick={() =>
+            confirm(`${post.id}번 글을 정말로 삭제하시겠습니까?`) &&
+            deletePost(post.id)
+          }
         >
           삭제
         </button>
@@ -48,6 +52,22 @@ export default function Page() {
           수정
         </Link>
       </div>
+
+      <h2>댓글 목록</h2>
+
+      {postComments == null && <div>댓글 로딩중...</div>}
+
+      {postComments != null && postComments.length == 0 && (
+        <div>댓글이 없습니다.</div>
+      )}
+
+      {postComments != null && postComments.length > 0 && (
+        <ul>
+          {postComments.map((comment) => (
+            <li key={comment.id}>{comment.content}</li>
+          ))}
+        </ul>
+      )}
     </>
   );
 }
--- src/type/post.ts
+++ src/type/post.ts
@@ -1,8 +1,17 @@
 export type PostDto = {
   id: number;
+  createDate: string;
+  modifyDate: string;
   title: string;
 };
 
 export type PostWithContentDto = PostDto & {
+  content: string;
+};
+
+export type PostCommentDto = {
+  id: number;
+  createDate: string;
+  modifyDate: string;
   content: string;
 };
