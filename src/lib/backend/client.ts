12acaa0daef84337f8a70b410cfc2eb74fe0e69c
zunobk <junho4560@naver.com>
2025-12-11 14:04:40 +0900

    apiFetch에 Headers 클래스 도입하여 안정성 높힘

--- src/lib/backend/client.ts
+++ src/lib/backend/client.ts
@@ -2,10 +2,13 @@ const NEXT_PUBLIC_API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 export const apiFetch = (url: string, options?: RequestInit) => {
   if (options?.body) {
-    options.headers = {
-      ...options.headers,
-      "Content-Type": "application/json; charset=utf-8",
-    };
+    const headers = new Headers(options?.headers || {});
+
+    if (!headers.has("Content-Type")) {
+      headers.set("Content-Type", "application/json; charset=utf-8");
+    }
+
+    options.headers = headers;
   }
 
   return fetch(`${NEXT_PUBLIC_API_BASE_URL}${url}`, options).then((res) =>
