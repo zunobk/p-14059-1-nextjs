b9a77fa7ec40b791d6c6221dc0b825bca38a3aa1
zunobk <junho4560@naver.com>
2025-12-11 15:19:33 +0900

    댓글 삭제, 하지만 아직 UI에 바로 반영되지는 않고 웹 페이지 리프레시를 해야 함

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -22,6 +22,14 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
     });
   };
 
+  const deleteComment = (id: number, commentId: number) => {
+    apiFetch(`/api/v1/posts/${id}/comments/${commentId}`, {
+      method: "DELETE",
+    }).then((data) => {
+      alert(data.msg);
+    });
+  };
+
   useEffect(() => {
     apiFetch(`/api/v1/posts/${id}`).then(setPost);
 
@@ -64,7 +72,18 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
       {postComments != null && postComments.length > 0 && (
         <ul>
           {postComments.map((comment) => (
-            <li key={comment.id}>{comment.content}</li>
+            <li key={comment.id}>
+              {comment.content}
+              <button
+                className="p-2 rounded border"
+                onClick={() =>
+                  confirm(`${comment.id}번 댓글을 정말로 삭제하시겠습니까?`) &&
+                  deleteComment(id, comment.id)
+                }
+              >
+                삭제
+              </button>
+            </li>
           ))}
         </ul>
       )}
