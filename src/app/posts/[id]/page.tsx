e0c287e31c5934c51817bfd892c23a20126317a8
zunobk <junho4560@naver.com>
2025-12-11 12:28:36 +0900

    글 삭제 후, 메세지 출력 후 글 목록으로 이동

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -2,15 +2,29 @@
 
 import { apiFetch } from "@/lib/backend/client";
 import type { PostWithContentDto } from "@/type/post";
-import { use, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import Link from "next/link";
 
-export default function Page({ params }: { params: Promise<{ id: number }> }) {
-  const { id } = use(params);
+export default function Page() {
+  const { id: idStr } = useParams<{ id: string }>();
+  const id = Number(idStr);
+  const router = useRouter();
 
   const [post, setPost] = useState<PostWithContentDto | null>(null);
 
+  const deletePost = (id: number) => {
+    apiFetch(`/api/v1/posts/${id}`, {
+      method: "DELETE",
+    }).then((data) => {
+      alert(data.msg);
+      router.replace("/posts");
+    });
+  };
+
   useEffect(() => {
-    apiFetch(`/api/v1/posts/${id}`).then(setPost);
+    apiFetch(`/api/v1/posts/${id}`)
+      .then(setPost);
   }, []);
 
   if (post == null) return <div>로딩중...</div>;
@@ -22,6 +36,18 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
       <div>번호 : {post.id}</div>
       <div>제목: {post.title}</div>
       <div style={{ whiteSpace: "pre-line" }}>{post.content}</div>
+
+      <div className="flex gap-2">
+        <button
+          className="p-2 rounded border"
+          onClick={() => deletePost(post.id)}
+        >
+          삭제
+        </button>
+        <Link className="p-2 rounded border" href={`/posts/${post.id}/edit`}>
+          수정
+        </Link>
+      </div>
     </>
   );
 }
