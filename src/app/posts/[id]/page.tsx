da69cc3378dc7b92c7c2a7ef313edbdea09196f4
zunobk <junho4560@naver.com>
2025-12-11 15:21:47 +0900

    댓글 목록에서 댓글 번호 노출, 댓글 작성 API 호출이 성공하면 UI에도 반영

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -61,6 +61,10 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
     }).then((data) => {
       alert(data.msg);
       contentTextarea.value = "";
+
+      if (postComments == null) return;
+
+      setPostComments([...postComments, data.data]);
     });
   };
 
@@ -120,7 +124,7 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
         <ul>
           {postComments.map((comment) => (
             <li key={comment.id}>
-              {comment.content}
+              {comment.id} : {comment.content}
               <button
                 className="p-2 rounded border"
                 onClick={() =>
